ccc8c2036b45440c23644d3e5e1921addac12aa0
Emil Norén <stratosfloor@hotmail.com>
2026-09-17 17:52:02 +0200

    make image not jump when load complete

--- app/page.tsx
+++ app/page.tsx
@@ -29,25 +29,26 @@ export default function Home() {
   const today = new Date().getDay();
   return (
     <div className='min-h-screen p-6 sm:p-10 md:p-12 font-[family-name:var(--font-geist-sans)]'>
-      <h1 className='text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-center font-bold mb-10 sm:mb-14'>
+      <h1 className='text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-center font-bold mb-6'>
         Dagens Hadoquote
       </h1>
       <main
         className={clsx(
-          'flex flex-col gap-8 md:gap-12 items-center justify-center',
+          'flex flex-col md:gap-12 items-center justify-center',
           'md:flex-row',
           today % 2 !== 0 && 'md:flex-row-reverse',
         )}
       >
-        {quote ? (
-          <h2 className='text-2xl sm:text-3xl md:text-4xl font-medium italic text-center max-w-2xl px-2'>
-            &quot;{quote}&quot;
-          </h2>
-        ) : null}
-
+        <div className='flex flex-col justify-center min-h-20'>
+          {quote ? (
+            <h2 className='text-2xl sm:text-3xl md:text-4xl font-medium italic text-center max-w-2xl px-2'>
+              &quot;{quote}&quot;
+            </h2>
+          ) : null}
+        </div>
         <Image
           className={clsx(
-            'w-full max-w-[320px] sm:max-w-[400px] md:max-w-[480px] lg:max-w-[540px] h-auto',
+            'w-full max-w-[320px] sm:max-w-100 md:max-w-120 lg:max-w-135] h-auto',
             today % 2 !== 0 && 'md:-scale-x-100',
           )}
           src='/imgs/captain-haddock_v2.jpg'
@@ -57,7 +58,7 @@ export default function Home() {
           priority
         />
       </main>
-      <div className='flex justify-end items-center sm:justify-center md:justify-end lg:justify-end xl:justify-end'>
+      <div className='flex justify-end items-center lg:justify-center xl:justify-end'>
         <LanguageSwitcher language={language} toggle={toggleLanguage} />
       </div>
     </div>
